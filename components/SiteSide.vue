3457fded826d06201f9adaf1d429eeaa17826e1a
tokka2 <tokka@ipon.biz>
2021-09-09 18:42:38 +0900

    add z-10

--- components/SiteSide.vue
+++ components/SiteSide.vue
@@ -1,6 +1,6 @@
 <template>
   <aside class="text-center p-4">
-    <div class="sticky top-2">
+    <div class="sticky top-2 z-10">
       <div class="flex justify-between">
         <span class="cursor-pointer" @click="movePrevMonth">
           <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
