7c65b708ba987f8cd21ce3a69d9be57fdfc5ff89
tokka2 <tokka2@users.noreply.github.com>
2022-06-04 02:53:26 +0000

    Deploying to gh-pages from  @ a01d911e1a3a147b1e189243ccd000310fad2419 🚀

---
+++ _nuxt/static/1654311061/posts/2022-04-06-0208/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1654311061",layout:"default",error:a,state:{targetUrl:"https:\u002F\u002Ftokka2.github.io\u002Ftokka_amazon_timesale",datetimes:[]},serverRendered:true,routePath:"\u002Fposts\u002F2022-04-06-0208",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
